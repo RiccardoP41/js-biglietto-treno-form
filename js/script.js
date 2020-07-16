06801c99f7e4b42af9651cebf1aa8be4fcf32fc3
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-16 16:05:28 +0200

    da mettere a posto il css

--- js/script.js
+++ js/script.js
@@ -6,33 +6,77 @@
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
 
-var nome = document.getElementById('nome-cognome').value;
-
-document.getElementById("nome-passeggero").innerHTML = nome;
-
-var carrozza = nRandom(1,9);
-var cP = nRandom(90000,99999);
-
-document.getElementById("carrozza").innerHTML = carrozza;
-document.getElementById("cp").innerHTML = cP;
-
-// caolcolo prezzo e categoria sconto
-var km = document.getElementById('km').value;
-var pb = km * 0.21;
-var categoria = document.getElementById('categoria').value;
-if (categoria == "cinno") {
-    pb *= 0.8;
-}else if (categoria == "senior") {
-    pb *= 0.6;
-}
-console.log(pb);
-document.getElementById("pb").innerHTML = pb;
+// ******PRIMA CREO TUTTO IN PARTI DIVERSE POI LO INSERISCO NELL'addEventListener
 
 
+// ****Nome
+
+// var nome = document.getElementById('nome-cognome').value;
+// document.getElementById("nome-passeggero").innerHTML = nome;
 
 
+// ****Numeri random
+
+// var carrozza = nRandom(1,9);
+// var cP = nRandom(90000,99999);//
+// document.getElementById("carrozza").innerHTML = carrozza;
+// document.getElementById("cp").innerHTML = cP;
+
+
+//****Prezzo e categoria sconto
+
+// var km = document.getElementById('km').value;
+// var pb = km * 0.21;
+// var categoria = document.getElementById('categoria').value;
+// var sconto = "";
+// if (categoria == "cinno") {
+//     pb * 0.8;
+//     sconto += "Sconto Minorenne"
+// }else if (categoria == "senior") {
+//     pb * 0.6;
+//     sconto += "Sconto Silver"
+// }else {
+//     sconto += "Paghi Tutto"
+// }
+// document.getElementById("pb").innerHTML = pb;
+// document.getElementById("sconto").innerHTML = sconto;
+
+document.getElementById("genera").addEventListener("click", function(){
+
+    var nome = document.getElementById('nome-cognome').value;
+    document.getElementById("nome-passeggero").innerHTML = nome;
+
+    var carrozza = nRandom(1,9);
+    var cP = nRandom(90000,99999);
+    document.getElementById("carrozza").innerHTML = carrozza;
+    document.getElementById("cp").innerHTML = cP;
+
+    var km = document.getElementById('km').value;
+    var pb = km * 0.21;
+    var categoria = document.getElementById('categoria').value;
+    var sconto = "";
+    if (categoria == "cinno") {
+        pb * 0.8;
+        sconto += "Sconto Minorenne"
+    }else if (categoria == "senior") {
+        pb * 0.6;
+        sconto += "Sconto Silver"
+    }else {
+        sconto += "Paghi Tutto"
+    }
+    document.getElementById("pb").innerHTML = pb;
+    document.getElementById("sconto").innerHTML = sconto;
+
+    biglietto.className = "show";
+})
+
+document.getElementById("annulla").addEventListener("click", function(){
+    biglietto.className = "hidden";
+})
 
 // funzioni
+
+// per i numeri random
 function nRandom(min,max){
     return Math.floor(Math.random()* (max - min+ 1) + min);
 }
